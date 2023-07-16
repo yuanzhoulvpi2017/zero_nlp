21692b16da4aba7a70320cd17e9e58075385d608
yuanzhoulvpi <yuanzhoulvpi@outlook.com>
2023-07-16 21:06:22 +0800

    update

---
+++ chatglm_v2_6b_lora/train.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES = 0 python main.py \
+    --do_train \
+    --train_file dataset/AdvertiseGen/train.json \
+    --validation_file dataset/AdvertiseGen/dev.json \
+    --preprocessing_num_workers 10 \
+    --prompt_column content \
+    --response_column summary \
+    --overwrite_cache \
+    --model_name_or_path chatglm2-6b_model \
+    --output_dir output/adgen-chatglm2-6b-lora_version \
+    --overwrite_output_dir \
+    --max_source_length 64 \
+    --max_target_length 128 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --predict_with_generate \
+    --max_steps 3000 \
+    --logging_steps 10 \
+    --save_steps 100 \
+    --learning_rate 2e-5 \
+    --lora_r 32
