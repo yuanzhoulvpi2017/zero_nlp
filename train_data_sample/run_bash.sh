4c1cd5ea413169e0edb4c1611ec247963f6bc57b
yuanzhoulvpi <yuanzhoulvpi@outlook.com>
2024-01-03 08:10:51 +0800

    update

---
+++ train_data_sample/run_bash.sh
@@ -0,0 +1,9 @@
+python run.py \
+    --dataset_dir \
+    data/test001data \
+    --cache_dir \
+    cache_data \
+    --output_dir \
+    testoutput001 \
+    --per_device_train_batch_size \
+    3
